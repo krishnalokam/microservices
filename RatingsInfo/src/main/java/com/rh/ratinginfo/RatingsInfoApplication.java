6793ef65b55899a4fbc094f98ef3a16a9ba7ce71
sivalokam <skkumarlokam@gmail.com>
2022-12-09 10:21:18 +0530

    Starter packages for 3 services

---
+++ RatingsInfo/src/main/java/com/rh/ratinginfo/RatingsInfoApplication.java
@@ -0,0 +1,13 @@
+package com.rh.ratinginfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingsInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingsInfoApplication.class, args);
+	}
+
+}
---
+++ RatingsInfo/src/test/java/com/rh/ratinginfo/RatingsInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rh.ratinginfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingsInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
